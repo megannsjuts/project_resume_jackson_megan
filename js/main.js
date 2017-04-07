25661264c016ae1953747e04d2bc78a5476f791c
Megan Jackson <meganjackson@ufl.edu>
2017-04-07 14:56:52 -0700

    add notes to main.js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// this is the tooltipster plugin, which adds a nice greeting when the user
+// hovers over my bio image.
+
 $(document).ready(function() {
 	$('.tooltip').tooltipster();
 
@@ -7,6 +10,11 @@ $('.tooltip').tooltipster({
 	theme: 'tooltipster-noir'
 });
 
+// this is custom jQuery code that adds interactivity to the page by customizing
+// the animate scroll plugin. When user clicks on the paragraph text in the
+// header, the page will automatically scroll the education section
+
 $('.scroll').click(function() {
+	// this is the animatescroll plugin
 	$('.education').animatescroll();
 });
